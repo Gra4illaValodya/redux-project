aedc3d96c3bd8a8129b415fca50b82cd37370793
Vova <gra4illa@gmail.com>
2024-07-17 09:11:33 +0300

    Use Hooks Redux

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,5 @@
 export const dec = () => ({ type: "DEC" });
 export const inc = () => ({ type: "INC" });
 export const rnd = () => {
-  const value = Math.floor(Math.random() * 100 + 1);
-  return { type: "RND", payload: value };
+  return { type: "RND", payload: Math.floor(Math.random() * 100 + 1) };
 };
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,21 @@
-import { connect } from "react-redux";
-import * as action from "../actions/actions";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { inc, dec, rnd } from "../actions/actions";
 import { bindActionCreators } from "redux";
-const Counter = ({ value, inc, dec, rnd }) => {
+
+const Counter = () => {
+  const counter = useSelector((state) => state.value);
+  const dispatch = useDispatch();
   return (
     <div className="Counter">
       <div className="wrapper">
-        <div className="count">{value}</div>
-        <div className="dec" onClick={dec}>
+        <div className="count">{counter}</div>
+        <div className="dec" onClick={() => dispatch(dec())}>
           DEC -
         </div>
-        <div className="inc" onClick={inc}>
+        <div className="inc" onClick={() => dispatch(inc())}>
           INC +
         </div>
-        <div className="rnd" onClick={rnd}>
+        <div className="rnd" onClick={() => dispatch(rnd())}>
           RND
         </div>
       </div>
@@ -20,11 +23,11 @@ const Counter = ({ value, inc, dec, rnd }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    value: state.value,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     value: state.value,
+//   };
+// };
 
 // const mapDispatchToProps = (dispatch) => {
 //   const { inc, dec, rnd } = bindActionCreators(action, dispatch);
@@ -35,4 +38,5 @@ const mapStateToProps = (state) => {
 //   };
 // };
 
-export default connect(mapStateToProps, action)(Counter);
+export default Counter;
+// connect(mapStateToProps, action)(Counter);
